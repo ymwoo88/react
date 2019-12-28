a0dc8b42c831261ba1de9520d072c123edc61672
ymwoo88 <ymwoo_join@naver.com>
2019-12-28 16:25:05 +0900

    기본 학습내용 커밋

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Contact from './Contact';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Contact/>
+    );
+  }
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
+import ContactCreate from './ContactCreate';
+
+import update from 'react-addons-update';
+
+export default class Contact extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedKey: -1,
+            keyword:'',
+            contactData: [{
+                name: 'Abet',
+                phone: '010-0000-0001'
+            }, {
+                name: 'Betty',
+                phone: '010-0000-0002'
+            }, {
+                name: 'Char',
+                phone: '010-0000-0003'
+            }, {
+                name: 'David',
+                phone: '010-0000-0004'
+            }]
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+    }
+
+    componentWillMount() {
+      const contactData = localStorage.contactData;
+
+      if (contactData) {
+        this.setState({
+          contactData: JSON.parse(contactData)
+        });
+      }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if(JSON.stringify(prevState.contactData) != JSON.stringify(this.state.contactData)) {
+        localStorage.contactData = JSON.stringify(this.state.contactData);
+      }
+    }
+
+    handleChange(e) {
+        this.setState({
+          keyword: e.target.value
+        });
+
+        console.log(key, 'is selected');
+    }
+
+    handleClick(key) {
+      this.setState({
+        selectedKey : key
+      });
+    }
+
+    handleCreate(contact) {
+      this.setState({
+          contactData: update(this.state.contactData, {$push: [contact]})
+      });
+    }
+
+    handleRemove() {
+      if (this.state.selectedKey < 0) {
+        return;
+      }
+
+      this.setState({
+          contactData: update(this.state.contactData,
+                        { $splice: [[this.state.selectedKey, 1]] }
+          ),
+          selectedKey: -1
+      });
+    }
+
+    handleEdit(name, phone) {
+      this.setState({
+        contactData: update(this.state.contactData,
+          {
+            [this.state.selectedKey]: {
+              name: {$set: name},
+              phone: {$set: phone}
+            }
+          })
+      });
+    }
+
+    render() {
+        const mapToComponents = (data) => {
+            data.sort();
+            data = data.filter((contact) => {
+                return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+              }
+            );
+            return data.map((contact, i) => {
+                return (<ContactInfo
+                          contact={contact}
+                          key={i}
+                          onClick={() => this.handleClick(i)}
+                        />);
+            });
+        };
+
+        return (
+            <div>
+                <h1>Contacts Page</h1>
+                <input
+                  name="keyword"
+                  placeholder="Srearch"
+                  value={this.state.keyword}
+                  onChange={this.handleChange}
+                />
+                <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails
+                  isSelected={this.state.selectedKey != -1}
+                  contact={this.state.contactData[this.state.selectedKey]}
+                  onRemove={this.handleRemove}
+                  onEdit={this.handleEdit}
+                />
+                <ContactCreate onCreate={this.handleCreate}
+                />
+
+            </div>
+        );
+    }
+}
